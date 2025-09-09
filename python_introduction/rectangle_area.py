0d9bdcddc3dc4fc571bc3f6dfbb6021787a4be27
Ayoub Bouabana <ayoubbouabana@gmail.com>
2025-09-09 17:27:35 +0100

    Add rectangle_area.py to calculate the area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,17 @@
+
+
+length = 10
+width = 5
+
+area = length * width
+print(f"The area of the rectangle is: {area}")
+
+
+hours = 2
+seconds = hours * 3600
+print(f"{hours} hour(s) is {seconds} seconds.")
+
+
+age = int(input("How old are you? "))
+future_age = age + 27
+print(f"In 2050, you will be {future_age} years old.")
