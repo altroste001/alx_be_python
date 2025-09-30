ab594a8084051cb41f6f581d867f961c85c554bd
Ayoub Bouabana <ayoubbouabana@gmail.com>
2025-09-30 18:17:37 +0100

    Add bank_account.py - Task 0: BankAccount class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, account_balance: float = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.account_balance >= amount:
+                self.account_balance -= amount
+                return True
+            else:
+                return False
+        else:
+            print("Withdrawal amount must be positive.")
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
