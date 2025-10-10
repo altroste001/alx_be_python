e405c72787f5ff0d77b87d09dbce1ef1d1ab9133
Ayoub Bouabana <ayoubbouabana@gmail.com>
2025-10-10 16:54:55 +0100

    Add __str__ method to Book class

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,7 +2,8 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        
+    def __str__(self):
+       return f"{self.title} by {self.author}"
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title,author)
