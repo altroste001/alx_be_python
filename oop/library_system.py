41094b8509543ea5e47c5a92c0cc0c512d15f16a
Ayoub Bouabana <ayoubbouabana@gmail.com>
2025-10-10 17:01:56 +0100

    Fix spacing and formatting in Library list_books output

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,8 +2,10 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+        
     def __str__(self):
-       return f"{self.title} by {self.author}"
+        return f"{self.title} by {self.author}"    
+        
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title,author)
@@ -14,24 +16,20 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
         
-class Library :
+class Library:
     def __init__(self):
         self.books = []
-        
-    def add_book(self,book):
-        self.books.append(book)  
-        
+
+    def add_book(self, book):
+        self.books.append(book)
+
     def list_books(self):
         for book in self.books:
             if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size} KB") 
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by{book.author}m Page count: {book.page_count}")   
-             
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
-                print(f"Book: {book.title} by {book.author}")          
-        
-                  
-                         
-        
-             
+                print(f"Book: {book.title} by {book.author}")
+
+
